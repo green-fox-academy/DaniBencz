dd7af1871f516aa8e8197f47d5b8e5654c0f90ce
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-17 17:26:26 +0100

    added cuboid.ts

---
+++ week-01/day-3/cuboid.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let height: number = 10;
+let width: number = 10;
+let depth: number = 10;
+
+let surfaceArea: number = 2*(width * depth + width * height + height * depth);
+let volume: number = depth * width * height;
+
+console.log("Surface Area: " + surfaceArea);
+console.log("Volume: " + volume);
