63229d990027f2129c35bf8ac597216738338709
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-17 15:01:22 +0100

    added coding-hours.ts

---
+++ week-01/day-3/coding-hours.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let daily = 6;
+let daysPerWeek= 5;
+let weeks = 17;
+let percent = daily*daysPerWeek/0.52;
+let p = percent.toFixed(1);
+
+console.log ("A Greenfox attendee codes on average " + daily * daysPerWeek * weeks + " hours during a semester.");
+console.log ("That is, they spend " + p + " percent of their weekly work-time coding.");
