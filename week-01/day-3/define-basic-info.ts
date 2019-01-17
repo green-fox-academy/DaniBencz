7bcb5ac04f6dca95fafa98d098c0d937a55c0688
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-17 16:08:56 +0100

    added define-basic-info.ts

---
+++ week-01/day-3/define-basic-info.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let myName: string = "Dani";
+let myAge: number = 32;
+let myHeight: number = 1.70;
+let ifMarried: Boolean = true;
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+console.log(ifMarried);
