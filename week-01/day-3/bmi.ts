b7c50c720ab5e2b4914ad3613744cd865c4ed3c6
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-17 15:57:04 +0100

    bmi.ts

---
+++ week-01/day-3/bmi.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+//chaneged values to my own ;)
+
+let massInKg: number = 67.5;
+let heightInM: number = 1.70;
+let BMI: number = massInKg/heightInM**2;
+
+//BMI = kg/m*m
+
+console.log(BMI.toFixed(1));
