46630acc7fae6eabbee7d7f32a9660a72118db82
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-18 12:48:55 +0100

    added two versions of print-even.ts

---
+++ week-01/day-4/print-even.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let a:number = 0;
+
+console.log(a);
+
+for(let i = 0; i < 250; i++){
+    a += 2;
+    console.log(a);
+}
+
+
+// Create a program that prints all the even numbers between 0 and 500
---
+++ week-01/day-4/print-even2.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let a:number = 0;
+
+console.log(a);
+
+for(a; a <= 500; a += 2){
+    console.log(a);
+}
