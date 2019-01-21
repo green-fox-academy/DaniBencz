fd50c635a0c01b3bb84731fd691399bf70fb77a4
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-21 15:39:16 +0100

    added sum-elements.ts

---
+++ week-02/day-1/sum-elements.ts
@@ -0,0 +1,11 @@
+'use strict'
+export{}
+
+let r = [54, 23, 66, 12];
+
+let x :number = r[1] + r[2];
+
+console.log(x);
+
+// -  Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+// -  Print the sum of the second and the third element
