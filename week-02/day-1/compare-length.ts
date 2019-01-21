765a4184c6e0e18c805a753c739b109177eb3e51
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-21 15:33:01 +0100

    added compare-lentgh.ts

---
+++ week-02/day-1/compare-length.ts
@@ -0,0 +1,18 @@
+'use strict'
+export{}
+
+let firstList = [1, 2, 3];
+let secondList = [4, 5];
+
+if(firstList.length < secondList.length){
+    console.log("second list has more elements");
+
+}   else {
+        console.log("first list has more elements");
+}
+
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
