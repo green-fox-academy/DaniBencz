d128637e276c586479c0b2c33fe750425b8346f6
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-21 16:20:37 +0100

    added swap-elements.ts

---
+++ week-02/day-1/swap-elements.ts
@@ -0,0 +1,10 @@
+'use strict'
+export{}
+
+let abc = ["Arthur", "Boe", "Chloe"];
+let cba = abc.reverse();
+
+console.log(cba);
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
