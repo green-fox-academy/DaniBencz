bef3f89e709ec40552b696371d1265fdc11eb8cd
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-21 15:17:32 +0100

    added print-params.ts

---
+++ week-02/day-1/print-params.ts
@@ -0,0 +1,13 @@
+'use strict'
+export{}
+
+function printParams (a, b, c = "hello", d?){
+    console.log(a, b, c, d);
+}
+
+printParams(3, "fox");
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
