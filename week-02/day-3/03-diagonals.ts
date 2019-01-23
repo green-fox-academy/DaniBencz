69e7e0bf72a603f3d27646775f6f6c4c42ded946
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-23 11:58:46 +0100

    finished 02-color-box.ts and 03-diagonals.ts

--- week-02/day-3/03-diagonals.ts
+++ week-02/day-3/03-diagonals.ts
@@ -5,5 +5,16 @@ const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
+function diagonals(x1, y1, x2, y2, color): any{
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
+
+diagonals(0, 0, 600, 400, "green");
+diagonals(600, 0, 0, 400, "red");
+
 // Draw the canvas' diagonals.
 // If it starts from the upper-left corner it should be green, otherwise it should be red.
