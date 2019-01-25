a348e2e72d7b957b087b714f1c2f27eda70f5b34
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-25 10:30:33 +0100

    finished 5-table-printer.ts

--- week-02/day-2/5-table-printer.ts
+++ week-02/day-2/5-table-printer.ts
@@ -1,4 +1,5 @@
 'use strict';
+
 // Create a function that prints the ingredient list of dictionaries to the console in the following format:
 //
 // +--------------------+---------------+----------+
@@ -14,6 +15,59 @@
 // OPTIONAL
 // The frist columns should be automatically as wide as the longest key
 
+function printIngr(myArray) {
+
+	//finds longest name value
+	let nameLength: number = 0;
+	for (let i: number = 0; i < myArray.length; i++) {
+		if (myArray[i].name.length > nameLength) {
+			nameLength = myArray[i].name.length + 1;
+		}
+	}
+
+	//adds spaces after each name
+	for (let i: number = 0; i < myArray.length; i++) {
+		while (myArray[i].name.length < nameLength) {
+			myArray[i].name = myArray[i].name + " ";
+
+		}
+	}
+
+	//draws the horizontal lines
+	let dashes: string = '---';
+	for (let i: number = 0; i < myArray.length; i++) {
+		while (dashes.length < nameLength) {
+			dashes = dashes + "-";
+		}
+	}
+	dashes = dashes + "+---------------+----------+";
+
+	//adds spaves to 'Ingredient' label
+	let ingredtient: string = "Ingredient"
+	while (ingredtient.length < nameLength) {
+		ingredtient = ingredtient + " ";
+	}
+
+	//--------------------------WE START DRAWING NOW----------------------------------
+
+	console.log("+-" + dashes);
+	console.log("| " + ingredtient + "| Needs cooling | In stock |");
+	console.log("+-" + dashes);
+
+	let cooling: string = "";
+	for (let i: number = 0; i < myArray.length; i++) {
+		if (myArray[i].needsCooling) {
+			cooling = "Yes";
+		} else {
+			cooling = "No ";
+		}
+		console.log("| " + myArray[i].name + "| " + cooling + 
+			"           |" + myArray[i].inStock + "         |");
+	}
+
+	console.log("+-" + dashes);
+}
+
 const ingredients: any[] = [
 	{ name: 'vodka', inStock: 1, needsCooling: true },
 	{ name: 'coffee_liqueur', inStock: 0, needsCooling: true },
@@ -24,3 +78,5 @@ const ingredients: any[] = [
 	{ name: 'lime juice', inStock: 0, needsCooling: true },
 	{ name: 'soda', inStock: 0, needsCooling: true }
 ];
+
+printIngr(ingredients);
