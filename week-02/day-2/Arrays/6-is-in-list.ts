d7cff63de0102a30e83088bb344b730b3dac7417
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-29 13:37:19 +0100

    updated 6-is-in-list.ts

--- week-02/day-2/Arrays/6-is-in-list.ts
+++ week-02/day-2/Arrays/6-is-in-list.ts
@@ -4,14 +4,24 @@
 // Create a function that accepts 'listOfNumbers' as an input
 // it should return "true" if it contains all, otherwise "false"
 
-let listOfNumbers: number[] = [2, 4, 6, 8, 10, 12, 14, 16];
+let listOfNumbers: number[] = [2, 4, 6, 8, 10, 12, 14, 17];
 
-function checkNums(anArray){
-  
-  for(let i: number = 0; i < anArray.length; i++){
-    if(anArray[i] === 4 || 8 || 12 || 16){
-      console.log(anArray[i]);
+function checkNums(anArray) {
+
+  //works with 'filter' too?
+  let howMany: number[] = [];
+  for (let i: number = 0; i < anArray.length; i++) {
+    if (anArray[i] === 4 || anArray[i] === 8 || anArray[i] === 12 || anArray[i] === 16) {
+      howMany.push(anArray[i]);
+      console.log(howMany);
     }
+  }
+  
+  //This will always return false, but why?
+  if (howMany === [4, 8, 12, 16]) {
+    console.log(true);
+  } else {
+    console.log(false);
   }
 }
 
