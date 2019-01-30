0ae1476a2eed9d49d3c3dddc9c6b920e38bde2e5
Daniel Bencz <daniel.bencz@gmail.com>
2019-01-30 11:57:07 +0100

    added post-it.ts

---
+++ week-03/day-3/post-it.ts
@@ -0,0 +1,20 @@
+
+class PostIt {
+
+  backgroundColor: string;
+  text: string;
+  textColor: string;
+
+  constructor(bgColor: string = 'szoveg', txt: string = 'szoveg', txtColor: string = 'szoveg') {
+    this.backgroundColor = bgColor;
+    this.text = txt;
+    this.textColor = txtColor;
+  }
+}
+
+const object1 = new PostIt('orange', 'Idea1', 'blue');
+const object2 = new PostIt('pink', 'Awesome', 'black');
+const object3 = new PostIt('yellow', 'Superb', 'green');
+const object4 = new PostIt();
+
+console.log(object4);
