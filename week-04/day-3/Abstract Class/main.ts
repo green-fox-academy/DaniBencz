ab5410ee6f71a0e7af9334745c6e8f5441b33c49
Daniel Bencz <daniel.bencz@gmail.com>
2019-02-06 16:01:27 +0100

    updated stringed instrument project

--- week-04/day-3/Abstract Class/main.ts
+++ week-04/day-3/Abstract Class/main.ts
@@ -8,14 +8,14 @@ let bassGuitar = new BassGuitar();
 let violin = new Violin();
 
 console.log('Test 1 Play');
-console.log(guitar.play());
-console.log(bassGuitar.play());
-console.log(violin.play());
+guitar.play();
+bassGuitar.play();
+violin.play();
 
 console.log('Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings .');
 let guitar2 = new ElectricGuitar(7);
 let bassGuitar2 = new BassGuitar(5);
 
 console.log('Test 2 Play');
-console.log(guitar2.play());
-console.log(bassGuitar2.play());
+guitar2.play();
+bassGuitar2.play();
--- week-04/day-3/Abstract Class/stringed-instruments.ts
+++ week-04/day-3/Abstract Class/stringed-instruments.ts
@@ -1,19 +1,17 @@
 'use strict';
 
 abstract class Instrument {
+  abstract name: string;
   abstract play();
 }
 
 abstract class StringedInstrument extends Instrument {
   numberOfStrings: number;
   abstract sound();
-  play() {
-    return this.sound();
-  }
 }
 
 class ElectricGuitar extends StringedInstrument {
-
+  name: string = 'ElectricGuitar';
   constructor(strings: number = 6) {
     super();
     this.numberOfStrings = strings;
@@ -22,10 +20,14 @@ class ElectricGuitar extends StringedInstrument {
   sound() {
     return 'Twang';
   }
+
+  play(): void {
+    console.log(`${this.name}, a ${this.numberOfStrings} instrument that goes ${this.sound()}`);
+  }
 }
 
 class BassGuitar extends StringedInstrument {
-
+  name: string = 'BassGuitar';
   constructor(strings: number = 4) {
     super();
     this.numberOfStrings = strings;
@@ -34,10 +36,13 @@ class BassGuitar extends StringedInstrument {
   sound() {
     return 'Duum-duum-duum';
   }
+  play(): void {
+    console.log(`${this.name}, a ${this.numberOfStrings} instrument that goes ${this.sound()}`);
+  }
 }
 
 class Violin extends StringedInstrument {
-
+  name: string = 'Violin';
   constructor(strings: number = 4) {
     super();
     this.numberOfStrings = strings;
@@ -45,6 +50,9 @@ class Violin extends StringedInstrument {
 
   sound() {
     return 'Screech';
+  }
+  play(): void {
+    console.log(`${this.name}, a ${this.numberOfStrings} instrument that goes ${this.sound()}`);
   }
 }
 
