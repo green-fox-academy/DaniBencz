9e442a5279f28ca2df42a08a2ec0cb645f507864
Daniel Bencz <daniel.bencz@gmail.com>
2019-02-27 11:31:28 +0100

    implemented first example

---
+++ week-06/day-3/littlePrince.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var king = document.getElementById('b325');
+console.log('king', king);
+var lamplighter = document.querySelector('.b329');
+console.log('lamplighter', lamplighter);
+var asteroids = document.querySelectorAll('div.asteroid');
+for (var i = 0; i < asteroids.length; i++) {
+    console.log('asteroid', asteroids[i]);
+}
