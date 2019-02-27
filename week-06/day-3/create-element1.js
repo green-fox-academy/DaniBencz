129b13a961f35f96e440230e88530b94607b4946
Daniel Bencz <daniel.bencz@gmail.com>
2019-02-27 17:03:44 +0100

    finished create-element-1 from exercise 5

--- week-06/day-3/create-element1.js
+++ week-06/day-3/create-element1.js
@@ -3,14 +3,24 @@
 console.log('foo');
 
 //Add an item that says 'The Green Fox' to the asteroid list.
-
+var greenFox = document.createElement('li');
+greenFox.innerText = 'The Green Fox';
+document.getElementsByClassName('asteroids')[0].appendChild(greenFox);
 
 //Add an item that says 'The Lamplighter' to the asteroid list.
-
+var lamplighter = document.createElement('li');
+lamplighter.textContent = 'The Lamplighter';
+document.querySelector('.asteroids').appendChild(lamplighter);
 
 //Add a heading saying 'I can add elements to the DOM!' to the.container.
-
+var newHeading = document.createElement('p');
+newHeading.id='heading';
+newHeading.textContent="Yo, I'm da headin'!";
+document.getElementsByTagName('body')[0].insertBefore(newHeading, document.querySelector('.asteroids'));
 
 //Add an image, any image, to the container.
-
+var newImage = document.createElement('img');
+newImage.id='flowres';
+newImage.src='./flowers.jpg';
+document.querySelector('.container').appendChild(newImage);
 
