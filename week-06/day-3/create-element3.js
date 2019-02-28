9ede9997219df200cdf0d9fcfd1a483d521eaaf4
Daniel Bencz <daniel.bencz@gmail.com>
2019-02-28 21:54:42 +0100

    finished exercise5

--- week-06/day-3/create-element3.js
+++ week-06/day-3/create-element3.js
@@ -14,15 +14,16 @@ const kids = [
   { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
 ];
 
-//1) Create an <article> element for each kid
+/*
+1) Create an <article> element for each kid
+2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
+- The H3 should contain the owner's name
+- The p should contain the pet's name
+3) Add the article to the pets div.*/
+
 kids.forEach(kid => {
   var newItem = document.createElement('article');
   document.getElementById('pets').appendChild(newItem);
-  newItem.textContent = kid.owner;
+  newItem.innerHTML = `<h3>owner: ${kid.owner}</h3>
+  <p>species: ${kid.petName}</p>`;
 });
-
-/* 2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
-    - The H3 should contain the owner's name
-    - The p should contain the pet's name */
-
-//3) Add the article to the pets div.
