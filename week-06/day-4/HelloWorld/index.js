9a48f4a0b1e5fbfea34ecf4f9c47c74ff5f1ca92
Daniel Bencz <daniel.bencz@gmail.com>
2019-02-28 13:52:30 +0100

    finished HelloWorld

--- week-06/day-4/HelloWorld/index.js
+++ week-06/day-4/HelloWorld/index.js
@@ -5,12 +5,15 @@ const app = express();
 const PORT = 3000;
 
 // set the view engine to ejs
+//Express will automatically look inside the views/ folder for template files
 app.set('view engine', 'ejs');
 
 // home page
 app.get('/', (req, res) => {
   // render `home.ejs`
-  res.render('home');
+  res.render('home', {
+    title: 'Hello World'
+  });
 });
 
 // start express app on port 3000
