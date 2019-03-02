3d6448ad9a054e18787d2981ca5f177993355205
Daniel Bencz <daniel.bencz@gmail.com>
2019-03-02 22:43:41 +0100

    went on with product-database exercise

--- week-02/day-2/Objects/extra/product-database.ts
+++ week-02/day-2/Objects/extra/product-database.ts
@@ -4,7 +4,7 @@
 keys are strings representing the product's name and the
 values are numbers representing the product's price.*/
 
-let products: {[key: string]: number} = {
+let products: { [key: string]: number } = {
   'Eggs': 200,
   'Milk': 200,
   'Fish': 400,
@@ -14,22 +14,55 @@ let products: {[key: string]: number} = {
 }
 
 //How much is the fish?
-
+console.log(`the fish costs ${products['Fish']}`);
 
 //What is the most expensive product?
-
+//first find highest price
+let highestPrice: number = 0;
+Object.values(products).forEach(price => {
+  if (price > highestPrice) {
+    highestPrice = price;
+  }
+})
+//find its key
+function getKeyByValue(object: object, value: number): string {
+  return Object.keys(object).find(key => object[key] === value);
+}
+console.log(`the most expensive product is ${getKeyByValue(products, highestPrice)}`);
 
 //What is the average price?
-
+let totalPrice: number = 0;
+Object.values(products).forEach(price => {
+  totalPrice += price;
+})
+console.log(`the average price is ${Math.floor(totalPrice / Object.values(products).length)}`);
 
 //How many products' price is below 300?
-
+let below300: number = 0;
+Object.values(products).forEach(price => {
+  if (price < 300) {
+    below300++;
+  }
+})
+console.log(`${below300} products' price is below 300`)
 
 //Is there anything we can buy for exactly 125?
-
+let exactly:boolean = false;
+Object.values(products).forEach(price => {
+  if (price === 125) {
+    exactly = true;
+  }
+})
+console.log(`we can buy something for exactly 125: ${exactly}`);
 
 //What is the cheapest product?
-
+let lowestPrice: number = 10000;
+Object.values(products).forEach(price => {
+  if (price < lowestPrice) {
+    lowestPrice = price;
+  }
+})
+console.log(`the cheepest product is ${getKeyByValue(products, lowestPrice)}`);
 
 //Which products cost less than 201? (just the name)
 
