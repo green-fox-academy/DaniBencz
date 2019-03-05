9966408e6e03d4a964a9761aaed0c157729b9f66
Daniel Bencz <daniel.bencz@gmail.com>
2019-03-05 14:59:02 +0100

    finished Doubling and Greeter exercise

--- week-07/Day-2/Server1/app.js
+++ week-07/Day-2/Server1/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 //set path
 const path = require('path');
 
@@ -11,11 +11,41 @@ app.get('/', (req, res) => {
 
 app.use('/assets', express.static('assets'));
 
-/* app.get('/assets', (req, res) => {
-  
-}); */
+app.get('/doubling', (req, res) => {
+  let input = req.query;
+  console.log(input);
+  if (input.input) {
+    res.json({
+      "received": input.input,
+      "result": input.input * 2
+    });
+  } else {
+    res.json({
+      "error": "Please provide an input!"
+    });
+  }
+});
 
-
+app.get('/greeter', (req, res) => {
+  let input = req.query;
+  if (input.name && input.title) {
+    res.json({
+      "welcome_message": `Oh, hi there ${input.name}, my dear ${input.title}!`
+    });
+  } else if (input.name && input.title === undefined) {
+    res.json({
+      "error": "Please provide a title!"
+    });
+  } else if (input.name === undefined && input.title) {
+    res.json({
+      "error": "Please provide a name!"
+    });
+  } else {
+    res.json({
+      "error": "Please provide a name and a title!"
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`server is running at ${PORT}`);
