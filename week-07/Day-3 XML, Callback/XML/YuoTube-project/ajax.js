4c965671f2ad777e0311dea9695521e4998bee69
Daniel Bencz <daniel.bencz@gmail.com>
2019-03-06 16:12:10 +0100

    finished YouTube code-along

--- week-07/Day-3 XML, Callback/XML/YuoTube-project/ajax.js
+++ week-07/Day-3 XML, Callback/XML/YuoTube-project/ajax.js
@@ -1,6 +1,9 @@
 'use strict';
 
-//gabbing the right <div>
+//helps make url dynamic
+let pageCounter = 1;
+
+//grabbing the right <div>
 let animalContainer = document.getElementById('animal-info');
 
 //set up button
@@ -10,28 +13,37 @@ btn.onclick = () => {
   let http = new XMLHttpRequest();
 
   //define request details
-  http.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json', true);
-  
-  http.onload = function(){
+  http.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`, true);
+
+  http.onload = function () {
     //like this, it will remain a string
     //let ourData = http.responseText;
     //to turn it into .json
     let ourData = JSON.parse(http.responseText);
-    renderHTML(http);
+    renderHTML(ourData);
   };
 
   //sending request
   http.send();
 
+  //incrementing page counter
+  //when out of content, button gets inactive
+  if (pageCounter < 3) {
+    pageCounter++;
+  } else {
+    btn.onclick = () => {
+    }
+  }
 };
 
-function renderHTML(data){
-  let htmlString = 'test';
-
-  for(i=0; i < data.length; i++){}
-    htmlString += '<p>'+data[]
-  }
+//creating .html content based on the .json data
+function renderHTML(data) {
+  let htmlString = '';
+  for (let i = 0; i < data.length; i++) {
+    //could go deeper based on video, this will do for now
+    htmlString = htmlString.concat(`<p>${data[i].name} is a ${data[i].species}.</p>`);
+  };
 
   //inserting content btwn the <div> tags
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
-}
+};
