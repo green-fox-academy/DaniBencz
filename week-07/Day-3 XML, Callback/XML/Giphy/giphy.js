3907bbae5ab8ebf70d94dc46b7e8d286c3eff5e9
Daniel Bencz <daniel.bencz@gmail.com>
2019-03-06 17:53:35 +0100

    finished Giphy task

--- week-07/Day-3 XML, Callback/XML/Giphy/giphy.js
+++ week-07/Day-3 XML, Callback/XML/Giphy/giphy.js
@@ -1,10 +1,26 @@
-let httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = console.log;
-httpRequest.open('GET', 'https://api.giphy.com/v1/stickers/search?q=spaceship&api_key=udaeAkvhx5NCotUJ70BCWc2khKVVZx5T&limit=16', true);
-httpRequest.send(null);
+'use strict';
 
-httpRequest.onload = () =>{
-  let data = JSON.parse(httpRequest.responseText);
-  console.log(data);
-  
+let div = document.querySelector('.giphs');
+let http = new XMLHttpRequest();
+
+http.open('GET',
+  'https://api.giphy.com/v1/stickers/search?q=spaceship&api_key=udaeAkvhx5NCotUJ70BCWc2khKVVZx5T&limit=16', true);
+http.send();
+
+http.onload = () => {
+  let data = JSON.parse(http.responseText);
+  renderHTML(data);
+}
+
+function renderHTML(data) {
+  console.log('foo');
+  console.log(data.data[0].images.downsized.url);
+  for (let i = 0; i < data.data.length; i++) {
+    //create new <img>
+    let img = document.createElement('img');
+    //assigns 'src' to it from data
+    img.setAttribute('src', data.data[i].images.downsized.url);
+    //insert in html
+    div.appendChild(img);
+  }
 }
