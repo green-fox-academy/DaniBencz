30db52e9e11aef5f03b0ddf461ec98cd0cdbb204
Daniel Bencz <daniel.bencz@gmail.com>
2019-03-11 17:05:39 +0100

    initiated candies exercise

--- week-08/day-1 EventListeners/candies.js
+++ week-08/day-1 EventListeners/candies.js
@@ -3,7 +3,38 @@
 /* Gather 10.000 candies!
 Clicking the ‘Create candies’ button gives you 1 candy.
 You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
-10 lollipops generate 1 candy per second.
+=======10 lollipops generate 1 candy per second.
 Use the lollipop emoji to display the lollipops you have
-Display the candy producton rate in the Candies / Second row
-If you press the "make candy rain" button, the candy generation should speed up 10x */
+=======Display the candy producton rate in the Candies / Second row
+=======If you press the "make candy rain" button, the candy generation should speed up 10x */
+
+let candies = 0;
+let lollies = 0;
+let lollyElm = document.querySelector('.lollypops');
+let candyElm = document.querySelector('.candies');
+let candyBtn = document.querySelector('.create-candies');
+let lollyBtn = document.querySelector('.buy-lollypops');
+let machineBtn = document.querySelector('.candy-machine');
+let lollyImage = String.fromCodePoint(0x1F36D);
+
+let clickCandy = (e) => {
+  candies++;
+  candyElm.innerText = candies;
+};
+
+let buyLolly = (e) => {
+  if (candies > 10) {
+    candies -= 10;
+    lollies++;
+    lollyElm.innerText += lollyImage;
+    candyElm.innerText = candies;
+  }
+};
+
+let machine = () => {
+  console.log('rain');
+};
+
+candyBtn.addEventListener('click', clickCandy);
+lollyBtn.addEventListener('click', buyLolly);
+machineBtn.addEventListener('click', machine);
