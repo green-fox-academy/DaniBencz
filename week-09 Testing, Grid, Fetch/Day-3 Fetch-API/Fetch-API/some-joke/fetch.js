b3b3dddfe93d8c245177e97f031541bcf842d35f
Daniel Bencz <daniel.bencz@gmail.com>
2019-03-20 16:00:09 +0100

    finished joke exercise - haha

--- week-09 Testing, Grid, Fetch/Day-3 Fetch-API/Fetch-API/some-joke/fetch.js
+++ week-09 Testing, Grid, Fetch/Day-3 Fetch-API/Fetch-API/some-joke/fetch.js
@@ -1,24 +1,22 @@
 'use strict';
 
 let btn = document.querySelector('button');
+let p = document.querySelector('p');
 
-let writeToDOM = () => {}
+let writeToDOM = (res) => {
+  //if want parse here, not in main fetch function:
+  res.json().then(res => p.innerText = res.value.joke);
+}
 
-let makeMeLaugh = (event) => {
-
+let makeMeLaugh = () => {
   fetch('http://api.icndb.com/jokes/random')
-    .then(function (response) {
-      console.log(response.json());
-    })
-    .catch(function (err) {
+    //.then(writeToDOM)
+    .then(res => res.json())
+    .then(res => p.innerText = res.value.joke)
+    .catch((err) => {
       console.log("Something went wrong!", err);
     });
 };
 
+//if () after function name, it gets called rightaway
 btn.addEventListener('click', makeMeLaugh);
-
-
-
-
-
-
